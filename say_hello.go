b929be1654bdbe16948aceb961ef09974bfb812a
ドニ_354 <doni.smpn1@gmail.com>
2023-12-03 00:17:11 +0700

    fixing bug

--- say_hello.go
+++ say_hello.go
@@ -1,10 +1,11 @@
 package go_say_hello
 import "fmt"
-func SayHello(string) {
-	var a string
+func SayHello(a string) {
+	
 	fmt.Println("Hello World!")
 	fmt.Println("What's your name?")
 	fmt.Scan(&a)
 	fmt.Println("My Name is ", a)
 	fmt.Println("Nice to see you", a)
+
 }
